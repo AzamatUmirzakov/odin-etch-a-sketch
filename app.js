86343602be86f59663017df590ad746d9a108eee
aumirzakov <azamat.umirzakoff@gmail.com>
2024-06-19 00:25:16 +0500

    Added hover-effects for grid squares

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 let container = document.querySelector(".container");
 let boardSize = 700;
 
+const colors = [
+    "#FF073A",  // Neon Red
+    "#FF61A6",  // Neon Pink
+    "#FF6FFF",  // Neon Fuchsia
+    "#FF00FF",  // Neon Magenta
+    "#FF66CC",  // Neon Hot Pink
+    "#FF9933",  // Neon Orange
+    "#FFD300",  // Neon Yellow
+    "#CCFF00",  // Neon Chartreuse
+    "#66FF00",  // Neon Lime Green
+    "#00FFCC"   // Neon Cyan
+];
+
+function random(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+
 function generateGrid(size) {
   if (size < 0) return;
   for (let i = 0; i < size ** 2; i++) {
@@ -8,8 +28,23 @@ function generateGrid(size) {
     square.classList.add("square");
     square.style.width = 700 / size + "px"
     square.style.height = 700 / size + "px"
+    square.style.backgroundColor = colors[random(0, 10)]
+    square.style.boxShadow = `0 0 50px 5px ${square.style.backgroundColor}`
     container.append(square);
   }
 }
+
+container.addEventListener('mouseover', (e) => {
+  let target = e.target;
+  if (target.classList.contains('square')) {
+    target.classList.add("hovered");
+  }
+});
+container.addEventListener('mouseout', (e) => {
+  let target = e.target;
+  if (target.classList.contains('square')) {
+    target.classList.remove("hovered");
+  }
+});
 
 generateGrid(16);
